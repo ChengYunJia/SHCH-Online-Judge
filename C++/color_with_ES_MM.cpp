6baeb891d9884548e91dada8df4f567e2a7327a2
“ChengYunJia” <dy101270@gmail.coml>
2024-01-31 16:01:20 +0800

    DP with matrix and exponentiating by squaring

--- C++/color_with_ES_MM.cpp
+++ C++/color_with_ES_MM.cpp
@@ -1,3 +1,4 @@
+//solving color problem with 'exponentiating by squaring' and 'matrix multiplication'
 #include<bits/stdc++.h>
 using namespace std;
 #define MM vector<vector<long long>>
@@ -8,10 +9,10 @@ using namespace std;
 int p = 3;
 int q = 3;
 int r = 3;
-MM IM((1,0),(0,1));
+MM IM = {{1,0,0},{0,1,0},{0,0,1}};
 
 //matrix multiplication
-MM mat(MM &x,MM &y)
+MM matrix_mat(MM x,MM y)
 {
     MM result(p,vector<long long>(r,0));//initialize a matrix(p*r) whose elements are "zero"
     for(int i = 0 ; i < p ; i++ )
@@ -20,16 +21,25 @@ MM mat(MM &x,MM &y)
         {
             for(int k = 0 ; k < q ; k++ )
             {
-                result[i][j] = x[i][k] * y[k][q];
+                result[i][j] = result[i][j] + x[i][k] * y[k][j];
             }
         }
     }
     return result;
 }
 
+//exponentiating by squaring but matrix version
 MM MatrixPow(MM x, long long y)
 {
-    MM result = IM
+    MM result = IM, tmp = x;
+    for( ; y ; y >>= 1, tmp = matrix_mat(tmp,tmp))
+    {
+        if( y & 1 )
+        {
+            result = matrix_mat(result,tmp) ;
+        }
+    }
+    return result;
 }
 
 
@@ -41,6 +51,16 @@ int main()
     //  | N-1 |  N  |
     //  |-----|-----|
 
+    int N;
+    cin >> N;
+
+    //now Blacks cannot be adjacent
+    MM transformation = {{1,1,1},{1,1,1},{1,1,0}};
+    MM start = {{1},{1},{1}};
+
+    start = matrix_mat( MatrixPow(transformation,N-1) , start );
+    int ans = start[0][0] + start[1][0] + start[2][0];
+    cout << ans << endl;
 
 
     return 0;
